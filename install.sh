62b365e8fd3b6effe8c9d7da12109955311f0137
Nelus <niels.beekhuis@gmail.com>
2019-05-17 12:50:03 +0200

    wrong file untarred

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ export LD_LIBRARY_PATH=/usr/local/cuda/lib64\${LD_LIBRARY_PATH:+:${LD_LIBRARY_PA
 
 echo "installing cudnn"
 wget http://projectsaturnus.area36.nl/cudnn-10.0-linux-x64-v7.5.1.10.tgz
-tar -xzvf cudnn-10.0-linux-x64-v5.1.tgz
+tar -xzvf cudnn-10.0-linux-x64-v7.5.1.10.tgz
 cp cuda/include/cudnn.h /usr/local/cuda/include
 cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
 chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
