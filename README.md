161329203126b91cd86f02a6e88ae52744092b14
NelusTheNerd <niels.beekhuis@gmail.com>
2019-05-17 10:50:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Project Saturnus
+Deze github bevat alle bestanden die gebruikt worden voor het HAN Smart industry project van Bluehub Innoveins. 
+
+Doel van dit project was om een applicatie te bouwen die onkruid kan herkennen. Om dit voor elkaar te krijgen is er gebruik gemaakt van Tensorflow en een voor getrained model van het Google Tensorflow team. 
+
+## Auteurs
+- Niels Beekhuis
---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+echo "starting install script"
+echo "========================="
+echo "installing drivers"
+add-apt-repository ppa:graphics-drivers/ppa 
+apt update
+apt-get -y install nvidia-375
+
+echo "installing cuda"
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+apt-get update
+apt-get -y install cuda
+
+echo "exporting path"
+export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64\${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
+echo "installing cudnn"
+wget http://projectsaturnus.area36.nl/cudnn-10.0-linux-x64-v7.5.1.10.tgz
+tar -xzvf cudnn-10.0-linux-x64-v5.1.tgz
+cp cuda/include/cudnn.h /usr/local/cuda/include
+cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
+chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
+
+echo "installing tensorflow"
+apt-get install libcupti-dev
+pip install tensorflow-gpu
+
+echo "getting the testscript"
