621eec38ee55a75f284cf4fe971a97a644aebff2
NelusTheNerd <niels.beekhuis@gmail.com>
2019-05-21 12:44:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,3 +50,6 @@ cd object_detection && protoc object_detection/protos/*.proto --python_out=.
 
 echo "python stuff"
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+
+echo "testing the object detection library"
+python object_detection/builders/model_builder_test.py
