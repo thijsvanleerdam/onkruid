d231d6d8e5f27e4f2e0388336bb8518550dcb3f3
Nelus <niels.beekhuis@gmail.com>
2019-05-22 14:07:20 +0200

    let saturnus do all the things

--- saturnus.sh
+++ saturnus.sh
@@ -27,3 +27,7 @@ docker run --runtime=nvidia -it --rm tensorflow/tensorflow:latest-gpu \
    python -c "import tensorflow as tf; tf.enable_eager_execution(); print(tf.reduce_sum(tf.random_normal([1000, 1000])))"
 
 mkdir -p output
+
+wget https://raw.githubusercontent.com/NelusTheNerd/symmetrical-octo-happiness/master/Dockerfile
+
+docker build -t trainer .
